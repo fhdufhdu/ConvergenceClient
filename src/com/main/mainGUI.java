75b4bef98b4668d8246e27c36296dc6c9dc0107c
fhdufhdu <fhdufhdu@gmail.com>
2020-12-02 20:32:44 +0900

    병합완료

--- src/com/main/mainGUI.java
+++ src/com/main/mainGUI.java
@@ -27,22 +27,10 @@ public class mainGUI extends Application
 	{
 		try
 		{
-
-<<<<<<< HEAD
-			String localHostAddress = InetAddress.getLocalHost().getHostAddress();
-
-			// String localHostAddress = "192.168.230.238";
-=======
 			// String localHostAddress = InetAddress.getLocalHost().getHostAddress();
 			String localHostAddress = "192.168.230.238";
->>>>>>> branch 'master' of https://github.com/fhdufhdu/ConvergenceClient
 			// Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 5000);
-<<<<<<< HEAD
-			Socket socket = new Socket("192.168.230.238", 5000);
-
-=======
 			Socket socket = new Socket(localHostAddress, 5000);
->>>>>>> branch 'master' of https://github.com/fhdufhdu/ConvergenceClient
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			launch();
