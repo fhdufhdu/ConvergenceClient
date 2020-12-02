bcfd871e69a2f5646db760dae809a9e7eda6920b
wowns1484 <74371211+wowns1484@users.noreply.github.com>
2020-12-02 20:54:36 +0900

    예매 내역 리스트 수정

--- src/com/main/mainGUI.java
+++ src/com/main/mainGUI.java
@@ -27,10 +27,10 @@ public class mainGUI extends Application
 	{
 		try
 		{
-			// String localHostAddress = InetAddress.getLocalHost().getHostAddress();
+//			String localHostAddress = InetAddress.getLocalHost().getHostAddress();
 			String localHostAddress = "192.168.230.238";
-			// Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 5000);
 			Socket socket = new Socket(localHostAddress, 5000);
+			
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			launch();
