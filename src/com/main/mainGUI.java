fa7ddf2c4e3b8fa0ff2a08ac7d2b7653a9d8b4b1
82103 <82103@DESKTOP-LS94SHP>
2020-12-02 17:10:26 +0900

    .

--- src/com/main/mainGUI.java
+++ src/com/main/mainGUI.java
@@ -31,7 +31,7 @@ public class mainGUI extends Application
 		{
 			DAO.connectDB();
 			String localHostAddress = InetAddress.getLocalHost().getHostAddress();
-			Socket socket = new Socket(localHostAddress, 5000);
+			Socket socket = new Socket("192.168.230.238", 5000);
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			launch();
